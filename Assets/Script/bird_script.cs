85e8506cd01ea060a774163c62ab1a52878ea4a8
gunawan885 <gunawanwi376@gmail.com>
2025-02-27 12:50:35 +0700

    adding ui and death

--- Assets/Script/bird_script.cs
+++ Assets/Script/bird_script.cs
@@ -6,19 +6,27 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public Rigidbody2D myriggidbody;
     public float flapstreght;
+    public logic_script logic;
+    public bool birdisalive = true;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.name = "Birds";
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<logic_script>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && birdisalive==true){
         myriggidbody.velocity = Vector2.up*flapstreght;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdisalive = false;
     }
 }
--- Assets/Script/logic_script.cs
+++ Assets/Script/logic_script.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class logic_script : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject GameOverScreen;
 
 [ContextMenu("increase Score")]
     public void addScore(int scoreToAdd){
         playerScore =playerScore+scoreToAdd;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("clicked");
+    }
+
+    public void gameOver (){
+        GameOverScreen.SetActive(true);
     }
 }
